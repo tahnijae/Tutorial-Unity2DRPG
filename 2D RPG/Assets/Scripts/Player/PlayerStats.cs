f9984e8f3124d9cfa0c31a693972d0c781c99961
Tahni <tahni.jae@gmail.com>
2024-04-11 18:43:05 -0700

    Added player experience basics

--- 2D RPG/Assets/Scripts/Player/PlayerStats.cs
+++ 2D RPG/Assets/Scripts/Player/PlayerStats.cs
@@ -18,10 +18,18 @@ public class PlayerStats : ScriptableObject
     public float Mana;
     public float MaxMana;
 
+    [Header("Exp")]
+    public float CurrentExp;
+    public float NextLevelExp;
+    public float InitialNextLevelExp;
+    [Range(1f,100f)] public float ExpMultiplier;
+
     public void ResetPlayer()
     {
         Health = MaxHealth;
         Mana = MaxMana;
-
+        Level = 1;
+        CurrentExp = 0f;
+        NextLevelExp = InitialNextLevelExp;
     }
 }
